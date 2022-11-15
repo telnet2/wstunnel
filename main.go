0a097364ba4d67083c73c7bed1151ceaedea5c71
Joohwi Lee <joohwi.lee@bytedance.com>
2022-11-15 09:12:42 -0800

    use args instead of config file

--- main.go
+++ main.go
@@ -2,40 +2,28 @@ package main
 
 import (
 	"flag"
-	"io/ioutil"
 	"os"
 	"os/signal"
 	"syscall"
 
 	log "github.com/fangdingjun/go-log/v5"
-	"gopkg.in/yaml.v2"
 )
 
 func main() {
-	var cfgfile string
-	var logfile string
 	var loglevel string
-	flag.StringVar(&cfgfile, "c", "config.yaml", "config file")
-	flag.StringVar(&logfile, "log_file", "", "log file")
+	var remoteAddr, localAddr string
+
+	flag.StringVar(&remoteAddr, "r", "", "remote WS url")
+	flag.StringVar(&localAddr, "l", "tcp://127.0.0.1:60060", "listening address (e.g., tcp://127.0.0.1:60060)")
 	flag.StringVar(&loglevel, "log_level", "INFO", "log level")
 	flag.Parse()
 
-	data, err := ioutil.ReadFile(cfgfile)
-	if err != nil {
-		log.Fatal(err)
+	if remoteAddr == "" {
+		log.Fatalf("-r remoteAddr is missing")
 	}
 
-	var cfg conf
-	if err := yaml.Unmarshal(data, &cfg); err != nil {
-		log.Fatal(err)
-	}
-
-	if logfile != "" {
-		log.Default.Out = &log.FixedSizeFileWriter{
-			MaxCount: 4,
-			Name:     logfile,
-			MaxSize:  10 * 1024 * 1024,
-		}
+	cfg := conf{
+		ProxyConfig: []proxyItem{{Listen: localAddr, Remote: remoteAddr}},
 	}
 
 	if lv, err := log.ParseLevel(loglevel); err == nil {
